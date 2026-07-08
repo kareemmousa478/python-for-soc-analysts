07264be41a467b85740a1b9cf6bde8a06a798857
Kareem Adel Mousa <kareemmousa478@gmail.com>
2026-07-08 20:08:42 +0300

    Update ip_validator.py

--- ip_validator.py
+++ ip_validator.py
@@ -1,9 +1,21 @@
 import ipaddress
 
-ip = input("Enter IP Address: ")
+ip = input("Enter an IP address: ")
 
 try:
-    ipaddress.ip_address(ip)
-    print("Valid IP Address")
+    ip_obj = ipaddress.ip_address(ip)
+
+    print(f"\nAddress: {ip_obj}")
+
+    if ip_obj.version == 4:
+        print("Version: IPv4")
+    else:
+        print("Version: IPv6")
+
+    print(f"Private : {ip_obj.is_private}")
+    print(f"Global  : {ip_obj.is_global}")
+    print(f"Loopback: {ip_obj.is_loopback}")
+    print(f"Multicast: {ip_obj.is_multicast}")
+
 except ValueError:
     print("Invalid IP Address")
